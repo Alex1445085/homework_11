9914f5bcdc23d492d17835027ffb4a846ba306bd
Alex1445085 <n1445085@yandex.ru>
2024-06-27 22:56:36 +0300

    Tasks 1,2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.time.Year;
+
 public class Main {
+
     public static void main(String[] args) {
 
-        System.out.println("Task #1.");
+        System.out.println("\nTask #1.");
         checkLeapYear(2022);
+
+        System.out.println("\nTask #2");
+        String os = "ios";
+        int osYear = 2023;
+        checkClientOsAndYear(os, osYear);
+
+        //System.out.println("\nTask #3.");
     }
 
     public static void checkLeapYear(int year) {
@@ -12,6 +22,20 @@ public class Main {
         } else {
             prefix = " не";
         }
-        System.out.printf("%d -%sвисокосный год.", year, prefix);
+        System.out.printf("%d -%sвисокосный год.\n", year, prefix);
+    }
+
+    public static void checkClientOsAndYear(String os, int year) {
+        int currentYear = Year.now().getValue();
+        if (os.equalsIgnoreCase("ios")) {
+            os = "iOS";
+        } else {
+            os = "Android";
+        }
+        String prefix = "обычную";
+        if (year < currentYear) {
+            prefix = "облегченную";
+        }
+        System.out.printf("Установите %s версию приложения для %s по ссылке\n", prefix, os);
     }
 }
