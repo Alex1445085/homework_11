b0bdc844e13d898cd8621f29265f782878db3f9e
Alex1445085 <n1445085@yandex.ru>
2024-06-27 23:28:49 +0300

    Tasks 1-3

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,19 @@ public class Main {
         int osYear = 2023;
         checkClientOsAndYear(os, osYear);
 
-        //System.out.println("\nTask #3.");
+        System.out.println("\nTask #3.");
+        int distanceToDelivery = 95;
+        int deliveryDays = calculateDeliveryDays(distanceToDelivery);
+        if (deliveryDays > 0) {
+            System.out.print("Доставка займёт " + deliveryDays);
+            if (deliveryDays == 1) {
+                System.out.println(" день");
+            } else {
+                System.out.println(" дня");
+            }
+        } else {
+            System.out.println("Доставки нет.");
+        }
     }
 
     public static void checkLeapYear(int year) {
@@ -37,5 +49,19 @@ public class Main {
             prefix = "облегченную";
         }
         System.out.printf("Установите %s версию приложения для %s по ссылке\n", prefix, os);
+    }
+
+    public static int calculateDeliveryDays(int distance) {
+        int days = 1;
+        if (distance > 20) {
+            days++;
+        }
+        if (distance > 60) {
+            days++;
+        }
+        if (distance > 100 || distance < 0) {
+            days = 0;
+        }
+        return days;
     }
 }
