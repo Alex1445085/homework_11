0464159e747f3057c1f6754be4b1e1e74f1c8f49
Alex1445085 <n1445085@yandex.ru>
2024-06-28 17:12:39 +0300

    Tasks 1-3 6th try

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,21 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("\nTask #1.");
-        checkLeapYear(2022);
+        checkLeapYear(1200);
 
         System.out.println("\nTask #2");
-        String os = "ios";
-        int osYear = 2024;
+        String os = "windows";
+        int osYear = 2018;
         checkClientOsAndYear(os, osYear);
 
         System.out.println("\nTask #3.");
         int distanceToDelivery = 95;
-        int deliveryDays = calculateDeliveryDays(distanceToDelivery);
-        if (deliveryDays > 0) {
-            System.out.print("Доставка займёт " + deliveryDays);
-            if (deliveryDays == 1) {
-                System.out.println(" день");
-            } else {
-                System.out.println(" дня");
-            }
-        } else {
-            System.out.println("Доставки нет.");
-        }
+        calculateDeliveryDays(distanceToDelivery);
     }
 
     public static void checkLeapYear(int year) {
         String prefix;
-        if (year >= 1584 && year % 4 == 0 && year % 100 !=0 || year % 400 == 0) {
+        if (year >= 1584 && (year % 4 == 0 && year % 100 !=0 || year % 400 == 0)) {
             prefix = " ";
         } else {
             prefix = " не";
@@ -39,8 +29,11 @@ public class Main {
         int currentYear = 2015;
         if (os.equalsIgnoreCase("ios")) {
             os = "iOS";
-        } else {
+        } else if (os.equalsIgnoreCase("android")) {
             os = "Android";
+        } else {
+            System.out.printf("У нас нет приложнеия для %s \n", os);
+            return;
         }
         String prefix = "обычную";
         if (year < currentYear) {
@@ -49,7 +42,7 @@ public class Main {
         System.out.printf("Установите %s версию приложения для %s по ссылке\n", prefix, os);
     }
 
-    public static int calculateDeliveryDays(int distance) {
+    public static void calculateDeliveryDays(int distance) {
         int days = 1;
         if (distance > 20) {
             days++;
@@ -60,6 +53,15 @@ public class Main {
         if (distance > 100 || distance < 0) {
             days = 0;
         }
-        return days;
+        if (days > 0) {
+            System.out.print("Доставка займёт " + days);
+            if (days == 1) {
+                System.out.println(" день");
+            } else {
+                System.out.println(" дня");
+            }
+        } else {
+            System.out.println("Доставки нет.");
+        }
     }
 }
